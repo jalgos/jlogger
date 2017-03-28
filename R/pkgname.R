f866d5397dff20773cd8bff0fac14397fc891813
Sébastien Lamy de la Chapelle <sebastien@jalgos.com>
2017-03-28 19:29:24 +0200

    Explicitely importing methods

--- R/pkgname.R
+++ R/pkgname.R
@@ -1,4 +1,5 @@
 #' @title Easily log messages
 #' @description A simple logger that prints messages to the console or to files using cat, print or write. Also compatible with pbdR.
 #' @template package.description
+#' @import methods
 "_PACKAGE"
