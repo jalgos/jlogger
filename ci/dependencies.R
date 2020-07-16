f84034061b6a91c67c373c3f569a8e30c3120dad
Ivan Diachenko <ivan@jalgos.com>
2020-07-16 13:15:07 +0200

    build: check if jsroot install transitive dependencies II

--- ci/dependencies.R
+++ ci/dependencies.R
@@ -4,11 +4,11 @@
 `.` <- list
 jspackages <- .("utils" = .(c('jconfig', version = "1.0.5")))
 
-# jsroot::dependencies(jspackages = jspackages,
-#                      cran.packages = .(
-#                          'RJSONIO'
-#                          #'track',
-#                          #'uuid'
-#                      ),
-#                      quiet = FALSE)
+jsroot::dependencies(jspackages = jspackages,
+                    #  cran.packages = .(
+                    #      'RJSONIO'
+                    #      #'track',
+                    #      #'uuid'
+                    #  ),
+                     quiet = FALSE)
 
